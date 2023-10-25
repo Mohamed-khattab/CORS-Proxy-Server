a2b6faa1b58ac041d63dff445c7c475295551edf
Mohamed-khattab <mohamed.e.khattab.0@gmail.com>
2023-10-25 16:11:20 +0300

    remove files

--- main.go
+++ main.go
@@ -8,39 +8,105 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
 type ModifyResponseWriter struct {
 	http.ResponseWriter
 }
 
-//optional :  modify in response part 
 func (w *ModifyResponseWriter) Write(b []byte) (int, error) {
 	// Add a custom response header
 	w.Header().Set("X-Custom-Response-Header", "Modified-Response")
 
-	// Modify the response body for HTML content
-	contentType := w.Header().Get("Content-Type")
-	if strings.Contains(contentType, "text/html") {
-		originalBody := string(b)
+	// Modify the response body based on a condition
+	originalBody := string(b)
+	modifiedBody := originalBody
 
-		// Inject a JavaScript script to alert a message
-		modifiedBody := fmt.Sprintf(`%s<script>alert('Modified Message');</script>`, originalBody)
-
-		// Write the modified response body
-		return w.ResponseWriter.Write([]byte(modifiedBody))
+	// Check if the original body contains a specific string
+	if strings.Contains(originalBody, "Hello") {
+		// If the string is found, replace it with a custom message
+		modifiedBody = strings.Replace(originalBody, "Hello", "Modified Hello", -1)
 	}
 
-	// For non-HTML content, write the original response body
-	return w.ResponseWriter.Write(b)
+	// Write the modified response body
+	return w.ResponseWriter.Write([]byte(modifiedBody))
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		// Serve the request
+		next.ServeHTTP(w, r)
+
+		// Log the request details
+		log.Printf(
+			"Method: %s\tURL: %s\tHeaders: %v\tStatus: %d\tDuration: %v\n",
+			r.Method, r.URL, r.Header, w.(ModifyResponseWriter).Status(), time.Since(start),
+		)
+	})
+}
+
+type rateLimiter struct {
+	mu        sync.Mutex
+	clients   map[string]int
+	rateLimit int
+	interval  time.Duration
+}
+
+func newRateLimiter(rateLimit int, interval time.Duration) *rateLimiter {
+	return &rateLimiter{
+		clients:   make(map[string]int),
+		rateLimit: rateLimit,
+		interval:  interval,
+	}
+}
+
+func (rl *rateLimiter) isAllowed(clientIP string) bool {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	now := time.Now()
+	count, exists := rl.clients[clientIP]
+	if !exists || now.Sub(time.Unix(int64(count), 0)) > rl.interval {
+		// Reset the counter if the interval has passed
+		rl.clients[clientIP] = int(now.Unix())
+		return true
+	}
+
+	// Increment the counter if within the interval
+	if count < rl.rateLimit {
+		rl.clients[clientIP]++
+		return true
+	}
+
+	return false
+}
+
+func rateLimitingMiddleware(rl *rateLimiter, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		clientIP := strings.Split(r.RemoteAddr, ":")[0]
+
+		if rl.isAllowed(clientIP) {
+			next.ServeHTTP(w, r)
+		} else {
+			http.Error(w, "Rate Limit Exceeded", http.StatusTooManyRequests)
+		}
+	})
 }
 
 func handleProxy(w http.ResponseWriter, r *http.Request) {
-	targetURL := r.Header.Get("Target-URL")
-	if targetURL == "" {
-		http.Error(w, "There is no Target-Endpoint header in the request", http.StatusInternalServerError)
+	// Your dynamic target URL configuration logic here
+	// For simplicity, let's set the target URL based on a query parameter
+	targetQueryParam := r.URL.Query().Get("target")
+	if targetQueryParam == "" {
+		http.Error(w, "Target query parameter is required", http.StatusBadRequest)
 		return
 	}
+
+	targetURL := targetQueryParam
 
 	// Create a reverse proxy
 	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
@@ -66,10 +132,19 @@ func main() {
 		port = "3000"
 	}
 
-	http.HandleFunc("/", handleProxy)
+	// Set up rate limiting with a limit of 10 requests per minute per IP
+	rl := newRateLimiter(10, time.Minute)
+
+	// Use the default ServeMux, which is a basic router
+	mux := http.NewServeMux()
+
+	// Register middleware
+	mux.HandleFunc("/", handleProxy)
+	handler := loggingMiddleware(rateLimitingMiddleware(rl, mux))
 
 	server := &http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
+		Handler: handler,
 	}
 
 	fmt.Printf("Proxy server listening on port %s\n", port)
